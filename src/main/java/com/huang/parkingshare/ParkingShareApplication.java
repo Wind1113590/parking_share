73addc2b174cf73446c70477bbcdddf8cd4ad2e4
Hong <h19359265517@qq.com>
2026-05-22 13:41:39 +0800

    导入初始依赖

---
+++ src/main/java/com/huang/parkingshare/ParkingShareApplication.java
@@ -0,0 +1,13 @@
+package com.huang.parkingshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huang/parkingshare/ParkingShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.parkingshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
