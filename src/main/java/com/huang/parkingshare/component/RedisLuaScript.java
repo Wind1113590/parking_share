92a62db2abd11ed539e615c5823e3f70512b1163
Hong <h19359265517@qq.com>
2026-05-24 21:16:48 +0800

    修改了一下RedisLuaScript里面的redisTemplate，发现好像不用抽出来lua模块。

--- src/main/java/com/huang/parkingshare/component/RedisLuaScript.java
+++ src/main/java/com/huang/parkingshare/component/RedisLuaScript.java
@@ -2,6 +2,7 @@ package com.huang.parkingshare.component;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -15,12 +16,13 @@ import java.util.List;
 @Component
 public class RedisLuaScript {
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+    @Qualifier("customStringRedisTemplate")
+    private RedisTemplate<String,String> redisTemplate;
 
     private DefaultRedisScript<Long> lockScript;
 
     @PostConstruct
-    public void init() {
+    public void init() {//这里其实也没必要把Lua脚本抽出来，一开始以为是Lua脚本的问题，后面发现没问题索性就不改了
         lockScript = new DefaultRedisScript<>();
         lockScript.setLocation(new ClassPathResource("lua/lock_time_slices.lua"));
         lockScript.setResultType(Long.class);
@@ -28,7 +30,7 @@ public class RedisLuaScript {
 
     public boolean lockTimeSlices(String key, List<Integer> startMinutes) {
         String[] args = startMinutes.stream().map(String::valueOf).toArray(String[]::new);
-        Long result = stringRedisTemplate.execute(
+        Long result = redisTemplate.execute(
                 lockScript,
                 Collections.singletonList(key),
                 (Object[]) args
